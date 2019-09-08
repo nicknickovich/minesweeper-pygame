949873fb87aa726a7d6c2a62a3fcc80a2e75244e
nicknickovich <50242565+nicknickovich@users.noreply.github.com>
2019-09-08 19:12:26 +0300

    Basic game features done

--- minesweeper.py
+++ minesweeper.py
@@ -21,22 +21,10 @@ pygame.display.set_caption("Minesweeper")
 DISPLAY_SURFACE = pygame.display.set_mode((settings.WINDOW_WIDTH, 
                                             settings.WINDOW_HEIGHT))
 
+game_over = False
+
 game_grid = GameGrid(DISPLAY_SURFACE)
-game_grid.generate_grid()
-game_grid.place_bombs()
-game_grid.place_numbers()
-
-last_tile = None
-
-def display_debug(tile):
-    if tile:
-        font = pygame.font.SysFont(None, 48)
-        text = font.render(", ".join([str(tile.x), str(tile.y), str(tile.pos_x), str(tile.pos_y), tile.type]), True, settings.WHITE)
-        text_rect = text.get_rect()
-        text_rect.center = ((settings.WINDOW_WIDTH / 2, settings.WINDOW_HEIGHT / 12 * 11))
-        DISPLAY_SURFACE.blit(text, text_rect)
-
-
+game_grid.setup_game_grid()
 
 while True:
     DISPLAY_SURFACE.fill(settings.BLACK)
@@ -48,13 +36,29 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == MOUSEBUTTONDOWN:
-            for tile in game_grid.tiles_map:
-                if (mouse_x > tile.x and mouse_x < tile.x + tile.TILE_SIZE
-                        and mouse_y > tile.y and mouse_y < tile.y + tile.TILE_SIZE):
-                    last_tile = tile
-
-    display_debug(last_tile)
+        if event.type == KEYDOWN:
+            if game_over and event.key == K_SPACE or event.key == K_RETURN:
+                game_over = False
+                game_grid.reset_grid()
+        
+        if not game_over:
+            if event.type == MOUSEBUTTONDOWN:
+                for row in game_grid.tiles_map:
+                    for tile in row:
+                        if (mouse_x > tile.x and mouse_x < tile.x + tile.TILE_SIZE
+                                and mouse_y > tile.y and mouse_y < tile.y + tile.TILE_SIZE):
+                            if pygame.mouse.get_pressed()[0] and tile.state == "closed":
+                                game_grid.open_around_blanks(tile)
+                                tile.state = "open"
+                                if tile.type == "bomb":
+                                    game_over = True
+                                    game_grid.reveal_all_bombs()
+                            elif pygame.mouse.get_pressed()[2]:
+                                if tile.state == "closed":
+                                    tile.state = "flag"
+                                elif tile.state == "flag":
+                                    tile.state = "closed"
+                        
     game_grid.draw_grid()
     pygame.display.update()
     fps_clock.tick(settings.FPS)
